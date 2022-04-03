2a8b05c37f84ec951b33056e8ed538ced61f9c7e
jambrose0 <jambrose0518@gmail.com>
2022-04-03 16:09:33 -0400

    add new products g2g

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,6 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // The `/api/products` endpoint
 
 // get all products
-// be sure to include its associated Category and Tag data
 router.get("/", (req, res) => {
   Product.findAll({
     include: [
@@ -23,10 +22,10 @@ router.get("/", (req, res) => {
 });
 
 // get one product
-// be sure to include its associated Category and Tag data
+
 router.get("/:id", (req, res) => {
   Product.findOne({
-    where: { id: req.body.id },
+    where: { id: req.params.id },
     include: [
       Category,
       {
@@ -47,15 +46,12 @@ router.get("/:id", (req, res) => {
 
 // create new product
 router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: req.body.tagIds,
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
