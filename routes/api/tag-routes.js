d66e7292f7cfdcb94ec8c422cea711a6313bd319
jambrose0 <jambrose0518@gmail.com>
2022-04-03 16:53:29 -0400

    can delete tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,37 +37,51 @@ router.get("/:id", (req, res) => {
 
 // create a new tag
 router.post("/", (req, res) => {
-  Tag.create({
-    tag_name: req.body.tag_name,
-    include: [Product],
-  })
-    .then((tag) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tag_name) {
-        const productId = req.body.tag_name.map((tag_name) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return Product.bulkCreate(productId);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((newTags) => res.status(200).json(newTags))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  // Tag.create({
+  //   tag_name: req.body.tag_name,
+  //   include: [Product],
+  // })
+  //   .then((tag) => {
+  //     // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+  //     if (req.body.tag_name) {
+  //       const productId = req.body.tag_name.map((tag_name) => {
+  //         return {
+  //           product_id: product.id,
+  //           tag_id,
+  //         };
+  //       });
+  //       return Product.bulkCreate(productId);
+  //     }
+  //     // if no product tags, just respond
+  //     res.status(200).json(product);
+  //   })
+  //   .then((newTags) => res.status(200).json(newTags))
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(400).json(err);
+  //   });
 });
 
+// update a tag's name by its `id` value
 router.put("/:id", (req, res) => {
-  // update a tag's name by its `id` value
+  // Tag.update;
 });
 
+// delete on tag by its `id` value
 router.delete("/:id", (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tags) => {
+      if (!tags) {
+        res.status(404).json({ message: "No tags found with this id" });
+        return;
+      }
+      res.json(tags);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
